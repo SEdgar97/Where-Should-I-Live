886eb8f3a959d18d8d2d1d95cbcf4ed07b5bde89
Michael Good <michael@Michaels-MacBook-Pro-3.local>
2021-02-03 19:02:03 -0500

    Fixing the map sizing and tile size

--- server.py
+++ server.py
@@ -5,17 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
-
-
-@app.route('/signUp')
-def signUp():
     return render_template('index.html')
 
 
 @app.route('/signUpUser', methods=['POST'])
 def signUpUser():
-    user =  request.form['username'];
+    user = request.form['username'];
     password = request.form['password'];
     return json.dumps({'status':'OK','user':user,'pass':password});
 
@@ -34,5 +29,5 @@ def getMapData(crime, healthcare, pollution, restaurant):
     return data
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run()
