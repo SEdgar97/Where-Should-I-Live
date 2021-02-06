52586f75972ec7a788cd39d565a73b015d888bc4
Michael Good <michael@Michaels-MacBook-Pro-3.local>
2021-02-06 00:45:25 -0500

    Updating the about page and cleaning up links

--- server.py
+++ server.py
@@ -15,6 +15,10 @@ def hello():
 def contact():
     return render_template('contact.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 @app.route('/signUpUser', methods=['POST'])
 def signUpUser():
